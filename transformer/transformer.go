f86117fefc0bf35dd8cadef7bdb0fbf4322e3cab
丁松杰 <377973147@qq.com>
2025-07-17 14:04:50 +0800

    删除无用的 信息

--- transformer/transformer.go
+++ transformer/transformer.go
@@ -3,7 +3,6 @@ package transformer
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/k8stech/alertmanager-wechatrobot-webhook/model"
@@ -50,8 +49,8 @@ func TransformToMarkdown(notification model.Notification, grafanaURL string, ale
 		// 动态获取 var-NameSpace 和 var-Container
 		namespace := labels["namespace"]
 		pod := labels["pod"]
-		instance := labels["instance"]
-		ip := strings.Split(instance, ":")
+		//instance := labels["instance"]
+		//ip := strings.Split(instance, ":")
 		// 获取告警等级
 		severity := labels["severity"]
 		// 获取对应的颜色
@@ -69,9 +68,9 @@ func TransformToMarkdown(notification model.Notification, grafanaURL string, ale
 		buffer.WriteString(fmt.Sprintf("\n>【内容】 %s\n", alert.Annotations["description"]))
 		buffer.WriteString(fmt.Sprintf("\n>【当前状态】%s \n", status))
 		buffer.WriteString(fmt.Sprintf("\n>【触发时间】 %s\n", cstTime.Format("2006-01-02 15:04:05")))
-		buffer.WriteString(fmt.Sprintf("\n [跳转Grafana看板](https://%s?orgId=1&var-origin_prometheus=&var-Node=%s&var-NameSpace=%s&var-Pod=%s&var-Pod=All)", grafanaURL, ip[0], namespace, pod))
+		// buffer.WriteString(fmt.Sprintf("\n [跳转Grafana看板](https://%s?orgId=1&var-origin_prometheus=&var-Node=%s&var-NameSpace=%s&var-Pod=%s&var-Pod=All)", grafanaURL, ip[0], namespace, pod))
 		buffer.WriteString(fmt.Sprintf("\n [告警规则详情](http://%s/alerts?search=)", alertDomain))
-		buffer.WriteString(fmt.Sprintf("\n [日志详情](https://aws-au-loki-grafana.vnnox.com/d/o6-BGgnnk/loki-kubernetes-logs?orgId=1&from=now-6h&to=now&var-query=&var-namespace=au&var-stream=All&var-container=vnnox-middle-oauth)"))
+		// buffer.WriteString(fmt.Sprintf("\n [日志详情](https://aws-au-loki-grafana.vnnox.com/d/o6-BGgnnk/loki-kubernetes-logs?orgId=1&from=now-6h&to=now&var-query=&var-namespace=au&var-stream=All&var-container=vnnox-middle-oauth)"))
 		buffer.WriteString(fmt.Sprintf("\n"))
 		buffer.WriteString(fmt.Sprintf("---------------------------------------------------------------------------\n"))
 	}
